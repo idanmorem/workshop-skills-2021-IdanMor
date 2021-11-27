5a6e964ee2b1ce4a2dfb758303326ba97572faaa
idanmorem <idanmorem@gmail.com>
2021-11-27 16:47:13 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Workshop_skills.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Workshop_skills.dir/main.cpp.obj"
+  "Workshop_skills.exe"
+  "Workshop_skills.exe.manifest"
+  "Workshop_skills.lib"
+  "Workshop_skills.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Workshop_skills.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
